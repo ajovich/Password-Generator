e765767d4e3984cf62d07e2a90252ffa7e933519
Allysha Jovich <ajovich@hotmail.com>
2021-04-09 20:50:21 -0700

    created empty array, updated boolean/if else statements

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 /* Pseudo Code
+Declare variables: passwordLength, uppercase, lowercase, numbers, symbols
+
+Declare empty array to store user input
+
 Prompt user to input desired length of password
-  IF length >= 8 || <= 128 then store in variable
-    ELSE alert 
+  if passwordlength >= 8 || passwordLength <= 128 
+    store within array
+  else alert "Please input again." 
+    writePassword(); function recall
+  END IF
 
+Prompt user to select cancel || confirm if they desire above criteria. Cancel = false/no, Confirm = true/yes. If none confirmed, alert window "You must confirm one"
+  if uppercase === true && lowercase === true && numbers === true && symbols === true 
+    store within array
+    else if ...
+    store within array
+    else if ... 
+    store within array
+    ...
+    else alert "Must select one"
+  END IF 
 
+Random math selection
+  for loop
 
+Return password
 */
-
-
-
-
-
-
 
 
 // Assignment Code.
@@ -37,8 +51,8 @@ var numbers = "0123456789";
 // Used \ to add certain special characters to the string.
 var symbols = " !\"#$%\&\'()*+,-./:;<=>?@[]^_`{|}~ ";
 
-
-
+// Empty array to store user input.
+securePassword = [];
 
 function writePassword() {
  
@@ -51,37 +65,17 @@ function writePassword() {
   }
 
   var uppercase = confirm("Would you like to use uppercase letters?");
+
+
+
   var lowercase = confirm("Would you like to use lowercase letters?");
+
+
+
   var numbers = confirm("Would you like to use numbers?");
+
+
+  
   var symbols = confirm("Would you like to use special characters?");
 
-
-
   }
-
- 
-
-
-
-/*
-// recursion???????? calling a function within a function
-// boolean confirm = true/yes, cancel = false/no
-
-//at bottom return() finished password
-
-
-// # of characters
-// lower case?
-// upper case?
-// symbols?
-// numbers?
-
-//Steps
-// Prompt to ask how many characters -- store within a variable 
-// Prompt do you want to use lowercase? boolean yes = true --> put it in variable
-// Same with uppercase, symbols, numbers, Use confirms for all of them 
-
-// can put all variables in a string or an array 
-// use random element out of an array 
-// use for loop to go # of characters employer wants 
-// */
